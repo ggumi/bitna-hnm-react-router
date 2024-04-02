f1a67f0f2be01b117988db16b668984febf4a3dc
YangGeumhee <rmagml9769@gmail.com>
2024-04-02 17:50:21 +0900

    feat : reducer를 여러 개 합치는 combineReducer 사용

--- src/page/Login.js
+++ src/page/Login.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 import { Container, Form, Button } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
+import {useDispatch} from "react-redux";
+import { authenticateAction } from "../redux/actions/authenticateAction";
+
 const Login = ({ setAuthenticate, to }) => {
+  const [id, setId]=useState('')
+  const [ password,setPassword]= useState('')
   console.log("tototo", to);
   const navigate = useNavigate();
+  const dispatch = useDispatch()
   const login = (event) => {
     event.preventDefault();
-    setAuthenticate(true);
+    // setAuthenticate(true);
+    dispatch(authenticateAction.login(id,password))
     navigate("/");
   };
   return (
@@ -14,12 +21,16 @@ const Login = ({ setAuthenticate, to }) => {
       <Form className="login-form" onSubmit={login}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control type="email" placeholder="Enter email"
+            onChange={(event)=>setId(event.target.value)}
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control type="password" placeholder="Password"
+            onChange={(event)=>setPassword(event.target.value)}
+          />
         </Form.Group>
 
         <Button variant="danger" type="submit">
