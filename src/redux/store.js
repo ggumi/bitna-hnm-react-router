b3e6cabd5884204eed2a7a414164c7ffed3d9474
YangGeumhee <rmagml9769@gmail.com>
2024-04-03 10:13:11 +0900

    chore : test

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,7 @@ import { createStore, applyMiddleware } from 'redux'
 import { composeWithDevTools } from "redux-devtools"
 import rootReducer from "./reducers"
 import {thunk} from 'redux-thunk'
+//
 
 let store = createStore(rootReducer, applyMiddleware(thunk) )
 export default store
